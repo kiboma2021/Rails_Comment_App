7177716f309ff10db6ea79bdd2f2d86b179048fd
kiboma <omayioben@gmail.com>
2023-07-10 07:34:19 +0300

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Post.destroy_all
+
+Post.create!([
+    {title: 'What is Post Comments?', body: 'A Post Comment, or simply Comment, is a primarily text-based form of engagement on a social media platform which allows users to respond to posts in their own words. The number of comments on a piece of content indicates the level of engagement users have with that content.'},
+    {title: 'How to calculate Post Comments', body: 'Say a business posts three pieces of content on Facebook in one day and each post receives 10 comments. The total number of comments on that day is 30.'},
+    {title: 'What is a good Post Comments benchmark?', body: 'According to a 2019 study of social media benchmarks from 20,000 customers across 12 industries by Sprout Social, brands receive an average of 101 comments on Facebook posts and an average of 69 comments on Instagram posts, per day across industries.'},
+    {title: 'How to visualize Post Comments?', body: 'Use a summary chart to measure your Post Comments data. This will let you compare your current data to a previous time period, letting you make adjustments to your strategy to engage your audience better.'},
+    {title: 'Things to comment on your friends post', body: 'It is worthwhile to make your friends happy as they read your message. What other better way than being a bit funny when crafting your comment on their pictures. Ensure you keep your comment precise.'},
+])
